413e82eb141d1b0cbe38767953bfbd5c51adcc93
Hakan Demir <141333685+kadirhakandemir@users.noreply.github.com>
2024-12-17 19:30:27 +0300

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://vital-bonefish-86.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
